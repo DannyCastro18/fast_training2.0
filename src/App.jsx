d9e6662095683c292b72a2af8f2a1557541c3625
Libni Bernate <libnibernate@gmail.com>
2024-06-20 17:39:12 -0500

    cambios contra

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import './App.css'
+import Codigo from './pages/entrenador/landing-page/Codigo'
+import Contrasena from './pages/entrenador/landing-page/Contrasena'
+import ChangePassword from './components/regist-inisesion-input/ChangePassword'
 
 function App() {
   return (
-    <>
-      
-    </>
+    <div className='w-full'>
+      <Codigo />
+      <Contrasena />
+      <ChangePassword />
+    </div>
   )
 }
 
