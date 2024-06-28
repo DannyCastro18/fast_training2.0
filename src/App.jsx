2e30ef9db77b808adeecec1bc01dd1e6aacd3885
Dpaola-G <Dpaolag17@gmail.com>
2024-06-28 18:19:39 -0500

    el color del texto no funciona

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
-import './App.css'
+import Info from "./components/Perfil/cuadro1";
+import Configuracion from "./components/configuración/cuadro2";
 
 function App() {
   return (
     <>
-      
+      <Info />
+      <Configuracion />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
