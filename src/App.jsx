5f8e95b836886b239aa858fd068117fcd791a01e
Johan Muñoz <johanmunoz319@gmail.com>
2024-07-04 16:33:06 -0500

    >:]

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,19 @@ import './App.css';
 import Header from './components/Header';
 import MainContent from './components/Main';
 import Footer from './components/Footer';
+import DashboardTrainer from './pages/entrenador/DashboardTrainer';
 
 function App() {
   return (
-    <div className="w-screen">
-      <Header />
-      <MainContent />
-      <Footer />
-    </div>
+    // <div className="w-screen">
+    //   <Header />
+    //   <MainContent />
+    //   <Footer />
+    // </div>
+
+    <>
+      <DashboardTrainer />
+    </>
   );
 }
 
---
+++ src/pages/entrenador/DashboardTrainer.jsx
@@ -0,0 +1,11 @@
+import NavbarTrainer from '../../components/NavbarTrainer'
+
+function DashboardTrainer() {
+    return (
+        <>
+            <NavbarTrainer />
+        </>
+    )
+}
+
+export default DashboardTrainer;
